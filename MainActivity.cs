4cf385931465fd2fc68da91c215a538281ffc2ba
munsra <piero.silvestri93@gmail.com>
2017-06-06 12:49:10 +0200

    Init Commit

---
+++ MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace TeoFaceApp
+{
+	[Activity(Label = "TeoFaceApp", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			Button btnRegistration = FindViewById<Button>(Resource.Id.btnRegistration);
+			btnRegistration.Click += BtnRegistration_Click;
+		}
+
+		void BtnRegistration_Click(object sender, System.EventArgs e)
+		{
+			Intent intent = new Intent(this, typeof(RegistrationActivity));
+			StartActivity(intent);
+		}
+	}
+}
+
---
+++ RegistrationActivity.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace TeoFaceApp
+{
+	[Activity(Label = "RegistrationActivity")]
+	public class RegistrationActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Create your application here
+			SetContentView(Resource.Layout.Registration);
+
+			Button btnDone = FindViewById<Button>(Resource.Id.btnDone);
+			btnDone.Click += BtnDone_Click;
+
+		}
+
+		void BtnDone_Click(object sender, EventArgs e)
+		{
+			var name = FindViewById<EditText>(Resource.Id.editTextName);
+			var surname = FindViewById<EditText>(Resource.Id.editTextSurname);
+			var email = FindViewById<EditText>(Resource.Id.editTextEmail);
+
+			if (name.Text == "" || surname.Text == "" || email.Text == "")
+			{
+				Toast.MakeText(this, "ciao", ToastLength.Short).Show();
+			}
+		}
+	}
+}
